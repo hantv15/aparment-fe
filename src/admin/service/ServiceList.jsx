8d0feb903c14587054b01ac3e5571b4a34f8e040
hantv15 <57491771+hantv15@users.noreply.github.com>
2022-03-07 19:06:25 +0700

    UI service

---
+++ src/admin/service/ServiceList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Content from "../../core/Content";
+
+const ServiceList = () => {
+  return (
+    <>
+      <Content title="Danh sách dịch vụ" subName="Dịch vụ"></Content>
+    </>
+  );
+};
+
+export default ServiceList;
