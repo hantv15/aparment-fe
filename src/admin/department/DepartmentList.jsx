5f3741e665930ee553827ef83fd329a13e3f00e2
hantv15 <57491771+hantv15@users.noreply.github.com>
2022-03-13 00:00:54 +0700

    Revert "Merge branch 'master' into hantv/thong_bao_chay"
    This reverts commit a8fb9448ea4329bf0599f81d530f592aaa3f333d, reversing
    changes made to dd99b91bf45512116d738fb7b4538bd77fe481a0.

--- src/admin/department/DepartmentList.jsx
+++ src/admin/department/DepartmentList.jsx
@@ -15,6 +15,7 @@ const DepartmentList = () => {
   const [apartments, setApartments] = useState([]);
   const [buildings, setBuildings] = useState([]);
   const [pageCount, setPageCount] = useState(0);
+  const [curPage, setCurPage] = useState(1);
   const [file, setFile] = useState({});
   const [filters, setFilters] = useState({
     page_size: 10,
@@ -67,6 +68,7 @@ const DepartmentList = () => {
     try {
       const getApartments = async () => {
         const { data } = await NoGetPage(paramNoPageSize);
+        console.log(data.data);
         const countData = Math.ceil(data.data.length / filters.page_size);
         setPageCount(countData);
       };
@@ -82,8 +84,6 @@ const DepartmentList = () => {
       const getApartments = async () => {
         const { data } = await get(paramString);
         setApartments(data.data);
-        // const countData = Math.ceil(data.data.length / filters.page_size);
-        // setPageCount(countData);
       };
       getApartments();
     } catch (error) {
@@ -122,6 +122,7 @@ const DepartmentList = () => {
   };
   const handlePageClick = (data) => {
     const currentPage = data.selected + 1;
+    setCurPage(currentPage);
     setFilters({
       ...filters,
       page: currentPage,
@@ -297,7 +298,9 @@ const DepartmentList = () => {
                     <tbody>
                       {apartments.map((department, index) => (
                         <tr key={department.id}>
-                          <th scope="row">{index + 1}</th>
+                          <th scope="row">
+                            {(curPage - 1) * filters.page_size + (index + 1)}
+                          </th>
                           <td>{department.apartment_id}</td>
                           <td>{department.building_id}</td>
                           <td>{department.square_meters}m2</td>
@@ -318,14 +321,11 @@ const DepartmentList = () => {
                               Chi tiết
                             </Link>
                             <Link
-                              className="btn btn-sm btn-outline-success btn-flat"
+                              className="ml-1 btn btn-sm btn-outline-success btn-flat"
                               to={`/admin/department/edit/${department.id}`}
                             >
                               Sửa
                             </Link>
-                            <button className="btn btn-sm btn-outline-danger btn-flat">
-                              Xóa
-                            </button>
                           </td>
                         </tr>
                       ))}
@@ -336,6 +336,7 @@ const DepartmentList = () => {
               <div className="row">
                 <div className="col-sm-12">
                   <ReactPaginate
+                    forcePage={0}
                     previousLabel={"previous"}
                     nextLabel={"next"}
                     breakLabel={"..."}
@@ -350,7 +351,6 @@ const DepartmentList = () => {
                     previousLinkClassName={"page-link"}
                     nextClassName={"page-item"}
                     nextLinkClassName={"page-link"}
-                    activeClassName={"active"}
                   />
                 </div>
               </div>
--- src/core/Menu.js
+++ src/core/Menu.js
@@ -85,7 +85,7 @@ const Menu = () => {
                   to="/admin/department"
                   className="nav-link"
                 >
-                  <i className="nav-icon fas fa-th" />
+                  <i className="nav-icon fas fa-building" />
                   <p>
                     Căn hộ
                     {/* <span className="right badge badge-danger">New</span> */}
@@ -99,7 +99,7 @@ const Menu = () => {
                   to="/admin/user"
                   className="nav-link"
                 >
-                  <i className="nav-icon fas fa-th" />
+                  <i className="nav-icon fas fa-users" />
                   <p>
                     Người dùng
                     {/* <span className="right badge badge-danger">New</span> */}
@@ -113,7 +113,7 @@ const Menu = () => {
                   to="/admin/service"
                   className="nav-link"
                 >
-                  <i className="nav-icon fas fa-th" />
+                  <i className="nav-icon fas fa-check" />
                   <p>
                     Dịch vụ
                     {/* <span className="right badge badge-danger">New</span> */}
@@ -157,7 +157,7 @@ const Menu = () => {
                   className="nav-link"
                   onClick={() => logout()}
                 >
-                  <i className="nav-icon fas fa-arrow-right-from-bracket" />
+                  <i className="nav-icon fa fa-share" />
                   <p>
                     Đăng xuất
                     {/* <span className="right badge badge-danger">New</span> */}
